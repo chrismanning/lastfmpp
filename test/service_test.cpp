3ca1581c22d250db2bad227623da59bf6beef8b0
Christian Manning <cmanning999@gmail.com>
2015-07-11 23:21:00 +0100

    Re-license to MIT

--- test/service_test.cpp
+++ test/service_test.cpp
@@ -1,3 +1,10 @@
+/**************************************************************************
+** Copyright (C) 2015 Christian Manning
+**
+** This software may be modified and distributed under the terms
+** of the MIT license.  See the LICENSE file for details.
+**************************************************************************/
+
 #include <catch.hpp>
 
 #include <iostream>
